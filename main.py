7b1dfeace8aed86e4b862c782be234a173a88b3d
mahmudurmahid <m.mahid@icloud.com>
2025-04-21 18:52:56 +0100

    added frontend for weather forecast dashboard (e.g. location, days, option)

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import streamlit as st
 
+st.title("Weather Forecast Dashboard")
+location = st.text_input("Location:")
+days = st.slider("Number of Days to Forecast", min_value=1, max_value=5,
+                 help="Select the number of days for the weather forecast.")
+option = st.selectbox("Select the Data to View", ("Temperature", "Sky"))
+st.subheader(f"{option} forecast for the next {days} days in {location}")
