3d3e3e3d0ab5c0ff0cdedf4c8c437ed19c496107
mahmudurmahid <m.mahid@icloud.com>
2025-04-22 14:26:34 +0100

    functionality for get weather data function

--- backend.py
+++ backend.py
@@ -1,4 +1,12 @@
-def get_weather_data(location, forecast_days, weather_type):
-    url = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+import os
+import requests
+
+def get_weather_data(location, forecast_days=None, weather_type=None):
+    api_key = os.getenv("openweather_api_key")
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={location}&appid={api_key}"
+    response = requests.get(url)
+    data = response.json()
     return data
 
+if __name__ == "__main__":
+    get_weather_data(location="London")
