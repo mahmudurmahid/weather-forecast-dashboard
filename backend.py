7565a7c59be91435b2f2e6bf3a3b29cb5ae1ea23
mahmudurmahid <m.mahid@icloud.com>
2025-04-22 16:28:37 +0100

    return temperature values or sky given the forecast days

--- backend.py
+++ backend.py
@@ -10,7 +10,14 @@ def get_weather_data(location, forecast_days=None, weather_type=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={location}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    number_values = 8 * forecast_days
+    filtered_data = filtered_data[:8*number_values]
+    if weather_type == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if weather_type == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 if __name__ == "__main__":
     print(get_weather_data(location="London"))
