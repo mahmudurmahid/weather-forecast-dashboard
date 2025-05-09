351a1e65400bcfd1fdc26ee4a2b7dddf927d7ebb
mahmudurmahid <m.mahid@icloud.com>
2025-05-09 10:30:48 +0100

    refactored code no functional change

--- backend.py
+++ backend.py
@@ -6,6 +6,9 @@ load_dotenv()
 
 
 def get_weather_data(location, forecast_days=None):
+    """
+    Fetches weather data from OpenWeather API for a given location and number of forecast days.
+    """
     api_key = os.getenv("openweather_api_key")
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={location}&appid={api_key}"
     response = requests.get(url)
